2360fae77d27f1966c61ebfed344155dca1bdc07
Mohammadbn511 <117743300+Mohammadbn511@users.noreply.github.com>
2025-05-23 02:38:50 +0330

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <img src="/coin.png" alt="Coin" style={{ width: '100px' }} />
+      <h1>PumpFarcaster</h1>
+      <p>Mini App on Farcaster built with Next.js</p>
+    </div>
+  )
+}
